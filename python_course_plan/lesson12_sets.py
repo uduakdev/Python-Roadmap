2bc436d987943b4180c9d42bfd00e82bfa285ec4
Uduakabasi Etim <194159697+uduakdev@users.noreply.github.com>
2026-09-02 06:13:08 +0100

    Start sets lessons

---
+++ python_course_plan/lesson12_sets.py
@@ -0,0 +1,72 @@
+# does not print duplicates
+# numbers = {1, 2, 2, 10, 3, 4, 5}
+# print(numbers)
+#
+# # it is unordered
+# fruits = {"apple", "banana", "cherry"}
+# print(fruits)
+
+# mixed = {"Uduakabasi", 17, True}
+# print(mixed)
+
+# Difference between empty dictionary and empty sets
+# empty_1 = {}
+# print(type(empty_1))
+#
+# empty_2 = set()
+# print(type(empty_2))
+
+# .add() method
+# fruits = {"Apple", "Banana"}
+# print(fruits)
+#
+# fruits.add("Mango")
+#
+# print(fruits)
+
+# .add() won't create duplicates
+# numbers = {4, 1, 3}
+# print(numbers)
+#
+# numbers.add(4)
+# numbers.add(2)
+#
+# print(numbers)
+
+# .remove() method
+# fruits = {"Apple", "Banana", "Mango"}
+# print(fruits)
+# fruits.remove("Banana")
+#
+# print(fruits)
+
+# .remove() produces an error when the item doesn't exist
+# fruits = {"Apple", "Banana", "Mango"}
+#
+# fruits.remove("Orange")
+
+# .discard() method
+# fruits = {"Apple", "Banana", "Mango"}
+# print(fruits)
+#
+# fruits.discard("Banana")
+#
+# print(fruits)
+
+# .discard() does not produce an error when the item doesn't exist
+# fruits = {"Apple", "Banana", "Mango"}
+# print(fruits)
+#
+# fruits.discard("Orange")
+
+# in      → checks if something exists
+# fruits = {"Apple", "Banana", "Mango"}
+#
+# print("Apple" in fruits)
+# print("apple" in fruits)
+
+# not in  → checks if something does NOT exist
+# fruits = {"Apple", "Banana", "Mango"}
+#
+# print("Apple" not in fruits)
+# print("apple" not in fruits)

12d38fcc4d8205060e53fd20be69f1215d49a72b
Uduakabasi Etim <194159697+uduakdev@users.noreply.github.com>
2026-09-05 12:12:08 +0100

    Learn function parameters and arguments

---
+++ python_course_plan/lesson13_functions_level2.py
@@ -0,0 +1,28 @@
+# parameters and arguments in functions
+# def greet(name):
+#     print("Hello", name)
+#
+# greet("Uduakabasi")
+# greet("Michael")
+# greet(6)
+
+# def introduce(name, age):
+#     print(f"My name is {name}")
+#     print("I am", age, "years old")
+#
+# introduce("Uduakabasi", 17)
+# introduce("Angel", 18)
+
+# def favorite_food(food):
+#     print("My favorite food is", food)
+#
+# favorite_food("Afang soup")
+# favorite_food("Jollof rice")
+# favorite_food("Fried rice")
+
+def student_info(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+student_info("Uduakabasi", 17, "Software Engineering")

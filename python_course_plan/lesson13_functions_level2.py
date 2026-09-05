489a208acdff212877357a612becf2bf2b0d48cf
Uduakabasi Etim <194159697+uduakdev@users.noreply.github.com>
2026-09-05 13:17:47 +0100

    Learn function return values

--- python_course_plan/lesson13_functions_level2.py
+++ python_course_plan/lesson13_functions_level2.py
@@ -20,9 +20,61 @@
 # favorite_food("Jollof rice")
 # favorite_food("Fried rice")
 
-def student_info(name, age, course):
-    print("Name:", name)
-    print("Age:", age)
-    print("Course:", course)
+# def student_info(name, age, course):
+#     print("Name:", name)
+#     print("Age:", age)
+#     print("Course:", course)
+#
+# sif = student_info("Uduakabasi", 17, "Software Engineering")
+#
+# print(sif)
 
-student_info("Uduakabasi", 17, "Software Engineering")
+# return value in functions
+# def add(a, b):
+#     return a + b
+#
+# result = add(5, 3)
+#
+# print(result)
+
+# def student_info(name, age, course):
+#     return name, age, course
+#
+# sif = student_info("Uduakabasi", 17, "Software Engineering")
+#
+# print(sif)
+# print(type(sif))
+#
+# name, age, course = student_info(
+#     "Uduakabasi", 17, "Software Engineering"
+# )
+#
+# print(name)
+# print(age)
+# print(course)
+
+# def calculate(a, b):
+#     return a + b
+#
+# result = calculate(10, 5)
+#
+# print(result)
+# print(result * 2)
+# print(result + 100)
+
+# def square(number):
+#     return number * number
+#
+# answer = square(7)
+#
+# print(answer)
+
+def add(a, b):
+    return a + b
+
+x = add(5, 10)
+y = add(x, 20)
+
+print(y)
+
+# print() displays a value. return gives a value back to the caller.
